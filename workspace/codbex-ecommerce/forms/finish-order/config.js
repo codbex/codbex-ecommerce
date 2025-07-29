4a3943cc98eb86d3ce036021d6988eb96eb88932
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-07-29 14:26:44 +0300

    add forms

---
+++ workspace/codbex-ecommerce/forms/finish-order/config.js
@@ -0,0 +1,8 @@
+const viewData = {
+    id: 'FinishOrder',
+    label: 'FinishOrder',
+    path: '/services/web/codbex-ecommerce/forms/finish-order/index.html'
+};
+if (typeof exports !== 'undefined') {
+    exports.getPerspective = () => viewData;
+}
---
+++ workspace/codbex-ecommerce/forms/review-order/config.js
@@ -0,0 +1,8 @@
+const viewData = {
+    id: 'ReviewOrder',
+    label: 'ReviewOrder',
+    path: '/services/web/codbex-ecommerce/forms/review-order/index.html'
+};
+if (typeof exports !== 'undefined') {
+    exports.getPerspective = () => viewData;
+}
