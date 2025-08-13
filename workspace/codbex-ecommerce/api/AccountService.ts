6428fed59f96d7814bd4afd838c444fa95bba3df
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-08-13 14:45:36 +0300

    Add api endpoints (#84)

--- workspace/codbex-ecommerce/api/AccountService.ts
+++ workspace/codbex-ecommerce/api/AccountService.ts
@@ -1,24 +1,35 @@
-import { Controller, Get } from "sdk/http";
+import { Controller, Get, Put, Post, response } from "sdk/http";
 import { query, sql } from 'sdk/db';
-
+import * as utils from './UtilsService';
 import { user } from 'sdk/security';
+import * as types from './types/Types';
 
-type Money = {
-    amount: number;
-    currency: string;
-};
+import { CustomerAddressRepository as CustomerAddressDao } from "codbex-partners/gen/codbex-partners/dao/Customers/CustomerAddressRepository";
+import { CustomerRepository as CustomerDao } from "codbex-partners/gen/codbex-partners/dao/Customers/CustomerRepository";
 
 @Controller
 class AccountService {
 
-    @Get("/account/addresses")
-    public addressesData() {
+    private readonly customerAddressDao;
+    private readonly customerDao;
 
-        const customer = mapCustomer(user.getName());
+    constructor() {
+        this.customerAddressDao = new CustomerAddressDao();
+        this.customerDao = new CustomerDao();
+    }
+
+    @Get("/account/addresses")
+    public addressesData(): types.AddressesResponse {
+
+        const customer: number = utils.mapCustomer(user.getName());
 
         const addressQuery = sql.getDialect()
             .select()
             .column('CUSTOMERADDRESS_ID')
+            .column('CUSTOMERADDRESS_FIRSTNAME')
+            .column('CUSTOMERADDRESS_LASTNAME')
+            .column('CUSTOMERADDRESS_EMAIL')
+            .column('CUSTOMERADDRESS_PHONE')
             .column('CUSTOMERADDRESS_ADRESSLINE1')
             .column('CUSTOMERADDRESS_ADDRESSLINE2')
             .column('CUSTOMERADDRESS_COUNTRY')
@@ -27,69 +38,46 @@ class AccountService {
             .column('CUSTOMERADDRESS_CUSTOMER')
             .column('CUSTOMERADDRESS_CUSTOMERADDRESSTYPE')
             .from('CODBEX_CUSTOMERADDRESS')
-            .where('CUSTOMERADDRESS_CUSTOMERADDRESSTYPE = ?')
             .where('CUSTOMERADDRESS_CUSTOMER = ?')
             .build();
 
-        const customerQuery = sql.getDialect()
-            .select()
-            .column('CUSTOMER_FIRSTNAME')
-            .column('CUSTOMER_LASTNAME')
-            .column('CUSTOMER_EMAIL')
-            .column('CUSTOMER_PHONE')
-            .from('CODBEX_CUSTOMER')
-            .where('CUSTOMER_ID = ?')
-            .build();
-
-        const customerResult = query.execute(customerQuery, [customer]);
-
-        const billingAddress = query.execute(addressQuery, [2, customer]).map(row => {
-
-            const country = mapCountry(row.CUSTOMERADDRESS_COUNTRY);
-            const city = mapCity(row.CUSTOMERADDRESS_CITY);
+        const allAddresses = query.execute(addressQuery, [customer]).map(row => {
+            const country = utils.mapCountry(row.CUSTOMERADDRESS_COUNTRY);
+            const city = utils.mapCity(row.CUSTOMERADDRESS_CITY);
 
             return {
                 id: row.CUSTOMERADDRESS_ID,
-                firstName: customerResult[0].CUSTOMER_FIRSTNAME,
-                lastName: customerResult[0].CUSTOMER_LASTNAME,
-                country: country,
-                addressLine1: row.CUSTOMERADDRESS_ADRESSLINE1,
+                firstName: row.CUSTOMERADDRESS_FIRSTNAME,
+                lastName: row.CUSTOMERADDRESS_LASTNAME,
+                country,
+                addressLine1: row.CUSTOMERADDRESS_ADDRESSLINE1,
                 addressLine2: row.CUSTOMERADDRESS_ADDRESSLINE2,
-                city: city,
+                city,
                 postalCode: row.CUSTOMERADDRESS_POSTALCODE,
-                phoneNumber: customerResult[0].CUSTOMER_PHONE,
-                email: customerResult[0].CUSTOMER_EMAIL
+                phoneNumber: row.CUSTOMERADDRESS_PHONE,
+                email: row.CUSTOMERADDRESS_EMAIL,
+                addressType: row.CUSTOMERADDRESS_CUSTOMERADDRESSTYPE
             };
         });
 
-        const shippingAddress = query.execute(addressQuery, [1, customer]).map(row => {
-            const country = mapCountry(row.CUSTOMERADDRESS_COUNTRY);
-            const city = mapCity(row.CUSTOMERADDRESS_CITY);
+        const shippingAddress: types.Address[] = allAddresses
+            .filter(a => a.addressType === 1)
+            .map(({ addressType, ...rest }) => rest);
 
-            return {
-                id: row.CUSTOMERADDRESS_ID,
-                firstName: customerResult[0].CUSTOMER_FIRSTNAME,
-                lastName: customerResult[0].CUSTOMER_LASTNAME,
-                country: country,
-                addressLine1: row.CUSTOMERADDRESS_ADRESSLINE1,
-                addressLine2: row.CUSTOMERADDRESS_ADDRESSLINE2,
-                city: city,
-                postalCode: row.CUSTOMERADDRESS_POSTALCODE,
-                phoneNumber: customerResult[0].CUSTOMER_PHONE,
-                email: customerResult[0].CUSTOMER_EMAIL
-            };
-        });
+        const billingAddress: types.Address[] = allAddresses
+            .filter(a => a.addressType === 2)
+            .map(({ addressType, ...rest }) => rest);
 
         return {
-            shippingAddress: shippingAddress,
-            billingAddress: billingAddress
+            shippingAddress,
+            billingAddress
         };
     }
 
     @Get("/account/details")
-    public accountData() {
+    public accountData(): types.AccountDetails {
 
-        const customer = mapCustomer(user.getName());
+        const customerId = utils.mapCustomer(user.getName());
 
         const customerQuery = sql.getDialect()
             .select()
@@ -102,21 +90,23 @@ class AccountService {
             .where('CUSTOMER_ID = ?')
             .build();
 
-        const customerResult = query.execute(customerQuery, [customer]);
+        const customerResult = query.execute(customerQuery, [customerId]);
+
+        const customer = customerResult[0];
 
         return {
-            firstName: customerResult[0].CUSTOMER_FIRSTNAME,
-            lastName: customerResult[0].CUSTOMER_LASTNAME,
-            phoneNumber: customerResult[0].CUSTOMER_PHONE,
-            email: customerResult[0].CUSTOMER_EMAIL,
-            creationDate: customerResult[0].CUSTOMER_CREATEDAT
+            firstName: customer.CUSTOMER_FIRSTNAME,
+            lastName: customer.CUSTOMER_LASTNAME,
+            phoneNumber: customer.CUSTOMER_PHONE,
+            email: customer.CUSTOMER_EMAIL,
+            creationDate: customer.CUSTOMER_CREATEDAT
         };
     }
 
     @Get("/account/orders")
-    public ordersData() {
+    public ordersData(): types.SalesOrder[] {
 
-        const customer = mapCustomer(user.getName());
+        const customerId = utils.mapCustomer(user.getName());
 
         const salesOrderQuery = sql.getDialect()
             .select()
@@ -129,92 +119,91 @@ class AccountService {
             .where('SALESORDER_CUSTOMER = ?')
             .build();
 
-        const salesOrderResults = query.execute(salesOrderQuery, [customer]);
+        const salesOrderResults = query.execute(salesOrderQuery, [customerId]);
 
-        const salesOrders = salesOrderResults.map(row => {
-            const currencyCode = mapCurrencyCode(row.SALESORDER_CURRENCY);
-            const status = mapStatus(row.SALESORDER_STATUS);
+        const salesOrders: types.SalesOrder[] = salesOrderResults.map(row => {
+            const currencyCode = utils.mapCurrencyCode(row.SALESORDER_CURRENCY);
+            const status = utils.mapStatus(row.SALESORDER_STATUS);
 
             return {
                 id: String(row.SALESORDER_ID),
                 creationDate: new Date(row.SALESORDER_DATE).toISOString(),
-                status: status,
+                status,
                 totalAmount: {
                     amount: row.SALESORDER_TOTAL,
-                    currency: currencyCode,
-                } as Money
+                    currency: currencyCode
+                }
             };
         });
 
         return salesOrders;
     }
-}
 
-function mapCustomer(identifier: string) {
-    const customerQuery = sql.getDialect()
-        .select()
-        .column('CUSTOMER_ID')
-        .from('CODBEX_CUSTOMER')
-        .where(`CUSTOMER_IDENTIFIER = ?`)
-        .build();
+    @Put("/account/address/:id")
+    public updateAddress(body: types.UpdateAddress, ctx: any) {
+        const addressId = ctx.pathParameters.id;
+        const userIdentifier = user.getName();
 
-    const customerResult = query.execute(customerQuery, [identifier]);
+        const customerFromAddress = utils.getCustomerFromAddress(addressId);
+        const loggedCustomer = utils.mapCustomer(userIdentifier);
 
-    return customerResult[0].CUSTOMER_ID;
-}
+        if (customerFromAddress !== loggedCustomer) {
+            ctx.status = 403;
+            return { error: "Forbidden" };
+        }
 
-function mapStatus(statusId: number) {
-    const salesOrderStatusQuery = sql.getDialect()
-        .select()
-        .column('SALESORDERSTATUS_NAME')
-        .from('CODBEX_SALESORDERSTATUS')
-        .where(`SALESORDERSTATUS_ID = ?`)
-        .build();
+        const addressToUpdate = {
+            Id: addressId,
+            Customer: loggedCustomer,
+            FirstName: body.firstName,
+            LastName: body.lastName,
+            Email: body.email,
+            Phone: body.phoneNumber,
+            Country: utils.countryToId(body.country),
+            City: utils.cityToId(body.city),
+            AddressLine1: body.addressLine1,
+            AddressLine2: body.addressLine2 || '',
+            PostalCode: body.postalCode
+        };
 
-    const salesOrderStatusResult = query.execute(salesOrderStatusQuery, [statusId]);
+        try {
+            this.customerAddressDao.update(addressToUpdate);
 
-    return salesOrderStatusResult[0].SALESORDERSTATUS_NAME;
-}
+            const updatedAddress = this.customerAddressDao.findById(addressId);
 
-function mapCurrencyCode(currencyId: number) {
+            ctx.status = 200;
+            return { success: true, address: updatedAddress };
+        } catch (error) {
+            console.error("Failed to update address:", error);
+            ctx.status = 500;
+            return { error: "Internal Server Error" };
+        }
+    }
 
-    const currencyQuery = sql.getDialect()
-        .select()
-        .column('CURRENCY_CODE')
-        .from('CODBEX_CURRENCY')
-        .where(`CURRENCY_ID = ?`)
-        .build();
+    @Post("/account/details")
+    public updateAccount(body: types.UpdateAccount) {
+        const loggedCustomer = utils.mapCustomer(user.getName());
 
-    const currencyResult = query.execute(currencyQuery, [currencyId]);
+        const accountToUpdate = {
+            Id: loggedCustomer,
+            FirstName: body.firstName,
+            LastName: body.lastName,
+            Name: `${body.firstName} ${body.lastName}`,
+            Phone: body.phoneNumber,
+            Email: body.email,
+        };
 
-    return currencyResult[0].CURRENCY_CODE;
-}
+        try {
+            this.customerDao.update(accountToUpdate);
 
+            const updatedAccount = this.customerDao.findById(loggedCustomer);
 
-function mapCountry(countryId: number) {
-
-    const countryQuery = sql.getDialect()
-        .select()
-        .column('COUNTRY_NAME')
-        .from('CODBEX_COUNTRY')
-        .where(`COUNTRY_ID = ?`)
-        .build();
-
-    const countryResult = query.execute(countryQuery, [countryId]);
-
-    return countryResult[0].COUNTRY_NAME;
-}
-
-function mapCity(cityId: number) {
-
-    const cityQuery = sql.getDialect()
-        .select()
-        .column('CITY_NAME')
-        .from('CODBEX_CITY')
-        .where(`CITY_ID = ?`)
-        .build();
-
-    const cityResult = query.execute(cityQuery, [cityId]);
-
-    return cityResult[0].CITY_NAME;
+            response.setStatus(response.OK);
+            return { success: true, data: updatedAccount };
+        } catch (error) {
+            console.error("Failed to update account:", error);
+            response.setStatus(response.INTERNAL_SERVER_ERROR);
+            return { error: "Internal Server Error" };
+        }
+    }
 }
