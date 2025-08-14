d2b42166c090513b22ab6a121d591787def42f7d
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-08-14 16:51:26 +0300

    add country name to get addresses endpoint

--- workspace/codbex-ecommerce/api/AccountService.ts
+++ workspace/codbex-ecommerce/api/AccountService.ts
@@ -45,14 +45,16 @@ class AccountService {
             .build();
 
         const allAddresses = query.execute(addressQuery, [customer]).map(row => {
-            const country = utils.mapCountry(row.CUSTOMERADDRESS_COUNTRY);
+            const countryCode = utils.getCountryCode(row.CUSTOMERADDRESS_COUNTRY);
+            const countryName = utils.getCountryName(row.CUSTOMERADDRESS_COUNTRY);
             const city = utils.mapCity(row.CUSTOMERADDRESS_CITY);
 
             return {
                 id: row.CUSTOMERADDRESS_ID,
                 firstName: row.CUSTOMERADDRESS_FIRSTNAME,
                 lastName: row.CUSTOMERADDRESS_LASTNAME,
-                country,
+                country: countryCode,
+                countryName: countryName,
                 addressLine1: row.CUSTOMERADDRESS_ADRESSLINE1,
                 addressLine2: row.CUSTOMERADDRESS_ADDRESSLINE2,
                 city,
